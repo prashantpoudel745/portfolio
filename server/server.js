15f0ce2c215d94203a240f90df079d47792f0051
hello <prashantpoudel745@gmail.com>
2024-11-30 19:15:41 +0545

    Initial commit

--- server/server.js
+++ server/server.js
@@ -3,15 +3,23 @@ import dotenv from "dotenv";
 import cors from "cors";
 import Contact from "./models/usermodel.js";
 import connection from "./connection/connectdb.js";
-
+import path from "path";
 const app = express();
 dotenv.config();
+const __dirname = path.dirname(
+  " C:UsersanilsOneDriveDocumentsjs\frontendportfoiloproject"
+);
 
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connection();
+
+app.use(express.static(path.join(__dirname, "project/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 app.post("/api/contact", async (req, res) => {
   const { email, username, message } = req.body;
   console.log(email, username, message);
